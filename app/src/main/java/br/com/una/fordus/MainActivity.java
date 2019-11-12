d7b98a6bbacb6c067345e7561cd9c70ea0572e99
Thiago Santana <thiago91098@gmail.com>
2019-11-12 19:11:57 -0300

    Initial commit

---
+++ app/src/main/java/br/com/una/fordus/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.una.fordus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
