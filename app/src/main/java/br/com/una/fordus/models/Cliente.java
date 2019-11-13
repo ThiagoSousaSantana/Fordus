fe71bc154494250736798a48b1545ebce0a2f0d7
welbertFC <welbert.jsj@gmail.com>
2019-11-12 21:12:34 -0300

    Tratativas de erro cliente

---
+++ app/src/main/java/br/com/una/fordus/models/Cliente.java
@@ -0,0 +1,48 @@
+package br.com.una.fordus.models;
+
+public class Cliente {
+
+    String nome;
+    int cpf;
+    int telefone;
+    String email;
+
+    public Cliente(String nome, int cpf, int telefone, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
